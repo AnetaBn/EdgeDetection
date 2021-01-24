4dacfcba7db5f3eac28c1d407193b79e016f9ad1
AnetaBn <73952373+AnetaBn@users.noreply.github.com>
2021-01-24 13:14:28 +0100

    Add files via upload
    komentarze

--- edgedetection/Mix.java
+++ edgedetection/Mix.java
@@ -1,6 +1,22 @@
 package edgedetection;
 
+/**
+ * Klasa obsługuje operację splotu obrazu z maską
+ * @author Anna Plęs
+ */
+
 public class Mix {
+
+    /**
+     * Metoda pobiera obraz (w skali szarości), wyznacza jądro i pozycję oraz stosuje splot w wybranej pozycji
+     * @param input Tablica reprezentująca obraz
+     * @param x Współrzędna x pozycji splotu
+     * @param y Współrzędna y pozycji splotu
+     * @param k Tablica dwuwymiarowa reprezentująca jądro
+     * @param kernelHeight Wysokość jądra
+     * @param kernelWidth Szerokość jądra
+     * @return output Nowa wartość piksela po splocie
+     */
 
     public static double pixelMix(double[][] input, int x, int y, double[][] k, int kernelWidth, int kernelHeight) {
         double output = 0;
@@ -11,6 +27,17 @@ public class Mix {
         }
         return output;
     }
+
+    /**
+     * Metoda pobiera tablicę 2D poziomów szarości oraz jądro i stosuje splot nad obszarem obrazu określonym przez szerokość i wysokość
+     * @param input Podwójna tablica 2D reprezentująca obrazy
+     * @param width Szerokość obrazu
+     * @param height Wysokość obrazu
+     * @param kernel Tablica dwuwymiarowa reprezentująca jądro
+     * @param kernelHeight Wysokość jądra
+     * @param kernelWidth Szerokość jądra
+     * @return output Tablica 2D reprezentująca nowy obraz
+     */
 
     public static double[][] mix2D(double[][] input, int width, int height, double[][] kernel, int kernelWidth, int kernelHeight) {
         int smallWidth = width - kernelWidth + 1;
@@ -29,14 +56,24 @@ public class Mix {
         return output;
     }
 
+    /**
+     * Metoda pobiera tablicę 2D poziomów szarości oraz jądro i stosuje splot nad obszarem obrazu określonym przez szerokość i wysokość i zwraca część obrazu wyjściowego
+     * @param input Podwójna tablica 2D reprezentująca obraz
+     * @param width Szerokość obrazu
+     * @param height Wysokość obrazu
+     * @param kernel Tablica dwuwymiarowa reprezentująca jądro
+     * @param kernelHeight Wysokość jądra
+     * @param kernelWidth Szerokość jądra
+     * @return large Tablica 2D reprezentująca nowy obraz
+     */
+
     public static double[][] mix2DEdge(double[][] input, int width, int height, double[][] kernel, int kernelWidth, int kernelHeight) {
         int smallWidth = width - kernelWidth + 1;
         int smallHeight = height - kernelHeight + 1;
         int top = kernelHeight / 2;
         int left = kernelWidth / 2;
 
-        double[][] small = mix2D(input, width, height,
-                kernel, kernelWidth, kernelHeight);
+        double[][] small = mix2D(input, width, height, kernel, kernelWidth, kernelHeight);
         double large[][] = new double[width][height];
         for (int j = 0; j < height; ++j) {
             for (int i = 0; i < width; ++i) {
@@ -51,14 +88,23 @@ public class Mix {
         return large;
     }
 
+    /**
+     * Metoda stosuje mix2DEdge dla tablicy wejściowej
+     * @param input Tablica reprezentująca obraz
+     * @param width Szerokość obrazu
+     * @param height Wysokość obrazu
+     * @param kernel Tablica 2D reprezentująca obraz
+     * @param kernelHeight Wysokość jądra
+     * @param kernelWidth Szerokość jądra
+     * @return output Tablica 2D reprezentująca nowy obraz
+     */
 
     public double[][] mixNext(double[][] input, int width, int height, double[][] kernel, int kernelWidth, int kernelHeight) {
         double[][] newInput = input.clone();
         double[][] output = input.clone();
 
         for (int i = 0; i < 1; ++i) {
-            output = mix2DEdge(newInput, width, height,
-                    kernel, kernelWidth, kernelHeight);
+            output = mix2DEdge(newInput, width, height, kernel, kernelWidth, kernelHeight);
             newInput = output.clone();
         }
         return output;
--- edgedetection/RunEdgeDetection.java
+++ edgedetection/RunEdgeDetection.java
@@ -2,7 +2,18 @@ package edgedetection;
 
 import java.io.IOException;
 
+/**
+ * Klasa wywołująca program za pomocą klasy EdgeDetectionUI
+ * @author Aneta Bień, Anna Plęs
+ */
+
 public class RunEdgeDetection {
+
+    /**
+     * Przykład użycia klasy EdgeDetectionUI
+     * @param args Nieużywany
+     * @exception IOException W przypadku błędu użytkownika wywołuje wyjątek
+     */
 
     public static void main(String[] args) throws IOException {
         System.out.println("Working Directory = " + System.getProperty("user.dir"));
